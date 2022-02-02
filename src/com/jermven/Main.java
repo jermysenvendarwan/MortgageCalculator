d98f4a04501eed2c24cbd36398e81aeab946a2a0
Jermysen Vendarwan <jermysenvendarwan@gmail.com>
2022-02-02 22:52:52 +0800

    Added validation check

--- src/com/jermven/Main.java
+++ src/com/jermven/Main.java
@@ -11,15 +11,35 @@ public class Main {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Principal: ");
-        int principal = scanner.nextInt();
+        int principal = 0;
+        float annualInterest = 0;
+        byte years = 0;
 
-        System.out.println("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        System.out.print("Principal ($100k - $1M): ");
+        principal = scanner.nextInt();
+
+        while (principal < 100_000 || principal > 1_000_000) {
+            System.out.print("Enter a number between 100,000 amd 1,000,000: ");
+            principal = scanner.nextInt();
+        }
+
+        System.out.print("Annual Interest Rate (0-5%): ");
+        annualInterest = scanner.nextFloat();
+
+        while (annualInterest > 5.0) {
+            System.out.print("Enter a number between 0 and 5%: ");
+            annualInterest = scanner.nextFloat();
+        }
+
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
-        System.out.println("Period (Years): ");
-        byte years = scanner.nextByte();
+        System.out.print("Period (1-35 years): ");
+        years = scanner.nextByte();
+
+        while (years < 1 || years > 35) {
+            System.out.print("Enter a number between 1 and 35: ");
+            years = scanner.nextByte();
+        }
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         // Mortgage means the amount of money to be paid monthly to pay off the home
@@ -36,7 +56,5 @@ public class Main {
 
         System.out.println("Mortgage: " + mortgageFormatted + "\nTotal Interest paid: " + totalInterestPaidFormatted
          + "\nTotal amount paid in " + numberOfPayments + " payments: " + totalAmountPaidFormatted);
-
-
     }
 }
